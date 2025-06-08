f83f6777dfc5870f05a994e941eb8eced7ffb1cd
Tauimi <qqdslk@gmail.com>
2025-06-08 22:52:14 +0300

    Add files via upload

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -8,6 +7,6 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? 
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query'] : [],
-  }).$extends(withAccelerate())
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
